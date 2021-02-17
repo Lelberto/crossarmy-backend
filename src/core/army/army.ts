e77ba853ec9cd785f54dfd45158ddbc3ffdc1912
Lelberto <jsurieux@gmail.com>
2021-02-18 00:45:35 +0100

    Add ID attribute

--- src/core/army/army.ts
+++ src/core/army/army.ts
@@ -8,15 +8,18 @@ import { Vector2 } from '../vector2';
  */
 export class Army {
 
+  public readonly id: string;
   public readonly size: Vector2;
   public entities: Entity[];
 
   /**
    * Creates a new army.
    * 
+   * @param id ID
    * @param size Size
    */
-  public constructor(size: Vector2) {
+  public constructor(id: string, size: Vector2) {
+    this.id = id;
     this.size = size;
     this.entities = [];
   }
