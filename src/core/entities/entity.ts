20f3060728d0effdd2c29cf9edf46f71f46e848a
Lelberto <jsurieux@gmail.com>
2021-02-19 19:13:07 +0100

    Change numbers to strings in entity type

--- src/core/entities/entity.ts
+++ src/core/entities/entity.ts
@@ -52,6 +52,6 @@ export interface EntityConfiguration {
  * Entity type enumeration.
  */
 export enum EntityType {
-  BARBARIAN = 0,
-  ARCHER = 1
+  BARBARIAN = 'barbarian',
+  ARCHER = 'archer'
 }
